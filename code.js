6cb2bbaf7450c3ec860d4b1119b868f70f968279
Guilherme Galvao <guilhermevigalvao@hotmail.com>
2021-09-26 16:11:54 -0300

    menu concluído

--- code.js
+++ code.js
@@ -1,1 +1,19 @@
 let div_menu = document.getElementById("div-menu");
+let mobileMenu = document.getElementById("nav-menu-mobile");
+let showMobileMenuNow = false;
+
+
+function showMobileMenu() {
+  showMobileMenuNow = !showMobileMenuNow;
+
+  if(showMobileMenuNow) {
+    mobileMenu.classList.remove("no-show");
+    mobileMenu.classList.add("show");
+  } else {
+    mobileMenu.classList.remove("show");
+    mobileMenu.classList.add("no-show");
+  }
+}
+
+
+console.log(mobileMenu.classList);
